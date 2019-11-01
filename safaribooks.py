801162558aa3f20e681a6406fc767e74816a389b
Zákányi Balázs <zakanyi@gmail.com>
2019-11-01 11:34:38 +0100

    Update safaribooks.py

--- safaribooks.py
+++ safaribooks.py
@@ -1011,8 +1011,10 @@ class SafariBooks:
             os.remove(zip_file + ".zip")
 
         shutil.make_archive(zip_file, 'zip', self.BOOK_PATH)
-        os.rename(zip_file + ".zip", os.path.join(self.BOOK_PATH, self.book_id) + ".epub")
-
+        #os.rename(zip_file + ".zip", os.path.join(self.BOOK_PATH, self.book_id) + ".epub")
+        os.rename(zip_file + ".zip", os.path.join(os.path.join(PATH, "Books"), "".join(self.escape_dirname(self.book_title).split(",")[:2]) + ".epub"))
+        shutil.rmtree(self.BOOK_PATH)
+		
 
 # MAIN
 if __name__ == "__main__":
